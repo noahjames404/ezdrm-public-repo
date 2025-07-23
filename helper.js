1af0b05464964bf7f7f63968c42c7b188a0ed36e
James <37204262+noahjames404@users.noreply.github.com>
2025-07-23 12:25:27 +0800

    test run

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ var browser = 'Non-DRM browser';
 var drmType = 'No DRM';
 
 // Replace the DASH and HLS URIs when you test your own content. 
-var dashUri = 'C:/Users/James/Downloads/shaka-player-dash-hls/example.mpd';
+var dashUri = 'https://raw.githubusercontent.com/noahjames404/ezdrm-public-repo/refs/heads/main/video/example.mpd';
 var hlsUri = 'https://na-fps.ezdrm.com/demo/ezdrm/master.m3u8';
 
 
